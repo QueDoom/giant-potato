898f8ecaf22cf1d6c9dcc9e6025636e828bd43d3
QueDoom <norton@totl.se>
2026-07-05 20:41:45 +0200

    Sewer

--- src/main/java/net/quedoom/giant_potato/datagen/ModModelProvider.java
+++ src/main/java/net/quedoom/giant_potato/datagen/ModModelProvider.java
@@ -17,6 +17,7 @@ public class ModModelProvider extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
         blockStateModelGenerator.family(ModBlocks.SMOOTH_POTATOES);
+        blockStateModelGenerator.family(ModBlocks.MASH_TEST);
 //        BlockStateModelGenerator.createSlabBlockState(ModBlocks.SMOOTH_POTATOES_SLAB, Identifier.of(GiantPotato.MOD_ID, "smooth_potatoes_slab_bottom"),
 //                Identifier.of(GiantPotato.MOD_ID, "smooth_potatoes_slab_top"), Identifier.of(GiantPotato.MOD_ID, "smooth_potatoes_slab_full"));
     }
@@ -25,7 +26,6 @@ public class ModModelProvider extends FabricModelProvider {
     public void generateItemModels(ItemModelGenerators itemModelGenerator) {
         itemModelGenerator.generateFlatItem(ModItems.TILLER, ModelTemplates.FLAT_HANDHELD_ITEM);
         itemModelGenerator.generateFlatItem(ModItems.WRENCH, ModelTemplates.FLAT_HANDHELD_ITEM);
-        itemModelGenerator.generateFlatItem(ModItems.CRUSHER_WHEEL, ModelTemplates.FLAT_ITEM);
         itemModelGenerator.generateFlatItem(ModFluids.MASH_BUCKET, ModelTemplates.FLAT_ITEM);
         itemModelGenerator.generateFlatItem(ModFluids.POISONOUS_MASH_BUCKET, ModelTemplates.FLAT_ITEM);
         itemModelGenerator.generateFlatItem(ModFluids.SEWER_WATER_BUCKET, ModelTemplates.FLAT_ITEM);
